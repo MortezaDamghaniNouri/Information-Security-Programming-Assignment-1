072e7f32b97d51bbcb95717289ac21aeaa0905d2
Morteza Damghani <mortezadamghani77@gmail.com>
2021-11-16 07:12:34 -0800

    Assignment Finished.

--- Finding Open Ports of a Host/Finding Open Ports of a Host.py
+++ Finding Open Ports of a Host/Finding Open Ports of a Host.py
@@ -1,20 +1,23 @@
 import nmap
 
-host_ip = input("Enter the host IP you want to scan its ports: ")
-start_port_number = input("Enter the start port number: ")
-last_port_number = input("Enter the last port number: ")
-print("Processing...")
-nm = nmap.PortScanner()
-nm.scan(host_ip, start_port_number + "-" + last_port_number)
-for host in nm.all_hosts():
-    print('----------------------------------------------------')
-    for proto in nm[host].all_protocols():
-        print('----------')
-        print("Protocol: " + str(proto))
-        ports_list = nm[host][proto].keys()
-        for port in ports_list:
-            print('Port : %s\tState : %s' % (port, nm[host][proto][port]['state']))
-print("Ports scanning completed")
+while True:
+    host_ip = input("Enter the host IP you want to scan its ports or enter 'exit' to exit: ")
+    if host_ip == "exit":
+        break
+    start_port_number = input("Enter the start port number: ")
+    last_port_number = input("Enter the last port number: ")
+    print("Processing...")
+    nm = nmap.PortScanner()
+    nm.scan(host_ip, start_port_number + "-" + last_port_number)
+    for host in nm.all_hosts():
+        print('----------------------------------------------------')
+        for proto in nm[host].all_protocols():
+            print('----------')
+            print("Protocol: " + str(proto))
+            ports_list = nm[host][proto].keys()
+            for port in ports_list:
+                print('Port : %s\tState : %s' % (port, nm[host][proto][port]['state']))
+    print("Ports scanning completed")
 
 
 
